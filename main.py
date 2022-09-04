f3ef572e47109aa5abec5e74a4c32262ce10a546
Danes <57474974+danesn@users.noreply.github.com>
2022-09-04 15:20:05 +0000

    add user input and learn about dictionary

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def get_choices():
-    player_choice = "rock"
+    
+    player_choice = input("Enter a choice (rock, paper, scissor): ")
     computer_choice = "paper"
-
-    return player_choice
-
+  
+    choices = {
+      "player": player_choice,
+      "computer": computer_choice
+    }
+    return choices
 
 choices = get_choices()
-
 print(choices)
