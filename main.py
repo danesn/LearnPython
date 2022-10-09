90f59261ad3e4fa05dbcc6e8d3cc10ee9b6a6e75
Danes <57474974+danesn@users.noreply.github.com>
2022-10-09 14:47:12 +0000

    Check win Function and refactor code

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
+import random
+
 def get_choices():
-    
     player_choice = input("Enter a choice (rock, paper, scissor): ")
-    computer_choice = "paper"
-  
-    choices = {
-      "player": player_choice,
-      "computer": computer_choice
-    }
+    options = ["rock", "paper", "scissor"]
+    computer_choice = random.choice(options)
+    
+    choices = {"player": player_choice, "computer": computer_choice}
     return choices
 
+def check_win(player, computer):
+    if player == computer:
+        return f"Player({player}) and Computer({computer}), Draw!"
+    
+    elif player == "rock":
+      if computer == "paper":
+          return f"Player({player}) and Computer({computer}), You Lose!"
+      else:
+          return f"Player({player}) and Computer({computer}), You Win!"
+          
+    elif player == "paper":
+      if computer == "rock":
+          return f"Player({player}) and Computer({computer}), You Win!"
+      else:
+          return f"Player({player}) and Computer({computer}), You Lose!"
+    
+    elif player == "scissor":
+      if computer == "rock":
+          return f"Player({player}) and Computer({computer}), You Lose!"
+      else:
+          return f"Player({player}) and Computer({computer}), You Win!" 
+
 choices = get_choices()
-print(choices)
+
+result = check_win(choices["player"], choices["computer"])
+
+print(result)
